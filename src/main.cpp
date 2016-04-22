e5ee6fd2c185b3e4b4bf974a17ec9094b2c425a2
manster161 <manster161@gmail.com>
2016-04-22 21:49:04 +0200

    new file:   src/lib/tinyGPS/TinyGPS.cpp 	new file:   src/lib/tinyGPS/TinyGPS.h 	modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,8 @@
 #include "lib/time/TimeAlarms.h"
 #include "sensors/ThermometerHumidity.h"
 #include "core/Configuration.h"
+
+
 //Controls::WaterControl* _waterControl;
 Sensors::MoustureSensor* _moistureSensor;
 Sensors::ThermometerHumidity* _thermometer;
@@ -16,29 +18,6 @@ Sensors::ThermometerHumidity* _thermometer;
 Inputs::Buttons* _buttons;
 Outputs::Leds* _leds;
 
-
-void setup()
-{
-  Serial.begin(9600);
-  // initialize LED digital pin as an output.
-  //_waterControl = new Controls::WaterControl(Pins::WATER_CONTROL_PIN);
-  _moistureSensor = new Sensors::MoustureSensor(Pins::MOISTURE_SENSOR_PIN);
-  _thermometer = new Sensors::ThermometerHumidity(Pins::THERMOMETER_SENSOR_PIN);
-  _leds = new Outputs::Leds();
-  //_tempSensor = new Sensors::Thermometer(Pins::THERMOMETER_SENSOR_PIN, 15, 25);
-}
-
-void loop()
-{
-    while (_buttons->isMoistureCalibrationPushed()){
-
-      _moistureSensor->updateCalibrationValues();
-    }
-
-
-
-    delay(Configuration::SERIAL_UPDATE_TIME);
-}
 
 void printValues() {
   Serial.print("Moisture : ");
@@ -51,4 +30,49 @@ void printValues() {
   Serial.print("Humidity: ");
   Serial.println(_thermometer->getHumidity(), DEC);
   Serial.println("");
+}
+
+void setup()
+{
+  Serial.begin(9600);
+  // initialize LED digital pin as an output.
+  //_waterControl = new Controls::WaterControl(Pins::WATER_CONTROL_PIN);
+  _moistureSensor = new Sensors::MoustureSensor(Pins::MOISTURE_SENSOR_PIN);
+  _thermometer = new Sensors::ThermometerHumidity(Pins::THERMOMETER_SENSOR_PIN);
+  _leds = new Outputs::Leds();
+  //_tempSensor = new Sensors::Thermometer(Pins::THERMOMETER_SENSOR_PIN, 15, 25);
+  //Alarm.timerRepeat(Configuration::SERIAL_UPDATE_TIME, printValues);
+  /*myLcd->begin(16, 2);
+  myLcd->home ();                   // go home
+  myLcd->print("Hello, ARDUINO ");
+  myLcd->setCursor ( 0, 1 );        // go to the next line
+  myLcd->print (" WORLD!  ");*/
+}
+
+void loop()
+{
+
+
+
+
+    delay(Configuration::SERIAL_UPDATE_TIME);
+
+    //lcd.print("Moisture level:");
+  /*   lcd.setCursor(0, 1);
+    lcd.print(_moistureSensor->getValue(), DEC);
+    delay(Configuration::SERIAL_UPDATE_TIME);
+lcd.setCursor(0, 0);
+    lcd.print("Temperature:");
+    lcd.setCursor(0, 1);
+    lcd.print(_thermometer->getTemp(), DEC);
+    delay(Configuration::SERIAL_UPDATE_TIME);
+    lcd.setCursor(0, 0);
+    lcd.println("Humididty:");
+    lcd.setCursor(0, 1);
+    lcd.print(_thermometer->getHumidity(), DEC);
+    delay(Configuration::SERIAL_UPDATE_TIME);*/
+
+
+    //printValues();
+
 }
